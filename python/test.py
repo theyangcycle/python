8f9d67fb95d7574a9fc32e7b4bf11328644dedc6
theyangcycle <jonahyang2413@gmail.com>
2026-02-23 21:00:40 -0600

    test

--- python/test.py
+++ python/test.py
@@ -1,16 +1,13 @@
-x = list(input())
-x.pop(-2)
-x.pop(-1)
-x = ''.join(x)
-x = int(x)
-if x % 100 in [11,12,13]:
-    print(f'{x}th')
-else:
-    if x % 10 == 1:
-        print(f'{x}st')
-    elif x % 10 == 2:
-        print(f'{x}nd')
-    elif x % 10 == 3:
-        print(f'{x}rd')
+a = int(input())
+for b in range(a):
+    speed, dist = list(map(float,input().split(':')))
+    if speed == 0:
+        print("SAFE")
     else:
-        print(f'{x}th')
+        time = dist/speed
+        if time <= 1:
+            print("SWERVE")
+        elif time <= 5:
+            print("BRAKE")
+        else:
+            print("SAFE")
